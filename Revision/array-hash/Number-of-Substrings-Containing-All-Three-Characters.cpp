83d8c916d874aaeeb8da93b4b15afc444de3b801
imshubhamgiri <skgiri569@gmail.com>
2026-06-30 23:06:13 +0530

    LeetCode(1358): solve  Number of Substrings Containing All Three Characters with optimised brute force

---
+++ Revision/array-hash/Number-of-Substrings-Containing-All-Three-Characters.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution{
+
+public:
+    int countOfSubstring(string s){
+        int n=s.size();
+        int count =0;
+        //Optimised Brute Force solution
+        for(int i=0;i<n;i++){
+            bool hasA=false, hasB=false, hasC=false;
+            for(int j=i;j<n;j++){
+                if(s[j]=='a') hasA=true;
+                else if(s[j]=='b') hasB=true;
+                else if(s[j]=='c') hasC=true;
+
+                if(hasA && hasB && hasC){
+                    count += (n-j);
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+
+};
+
+int main(){
+
+    Solution sol;
+    string s = "abcabc";
+    int count = sol.countOfSubstring(s);
+    cout << "Count of substrings containing all three characters: " << count << endl;
+}
